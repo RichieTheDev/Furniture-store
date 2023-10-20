100277a502a117cc6242115d8fad83c4eb902a6d
RichieTheDev <richiemomodu@gmail.com>
2023-10-20 11:53:21 +0100

    changes

--- app/components/Discover.tsx
+++ app/components/Discover.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
+// @ts-ignore
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 
@@ -19,9 +20,9 @@ const Discover = async () => {
 
   return (
     <>
-      <div className="bg-gray-200 h-[90vh]">
-        <div className="sm:w-[45%] pl-4 pt-8 sm:pl-16 sm:pt-32 lg:pt-48 sm:float-left">
-          <h1 className="sm:text-3xl text-3xl lg:text-4xl font-bold ">
+      <div className=" h-full sm:h-[90vh]">
+        <div className="sm:w-[45%] pl-4 pt-8 sm:pl-8 lg:pl-12 sm:pt-24 lg:pt-28 sm:float-left">
+          <h1 className="sm:text-4xl text-3xl  font-bold ">
             Discover Our
             <br />
             Featured Products
@@ -36,15 +37,25 @@ const Discover = async () => {
         <Splide
           options={{
             pagination: false,
-            perPage: 2.5,
+            perPage: 3,
             gap: "0.5rem",
-            rewind: true,
+            rewind: false,
             autoplay: true,
             arrows: true,
             type: "loop",
             drag: "free",
+            breakpoints: {
+              640: {
+                perPage: 2,
+                arrows: false,
+              },
+              768: {
+                perPage: 2,
+                arrows: false,
+              },
+            },
           }}
-          className="sm:w-[55%] sm:float-right sm:pt-10 pt-4 lg:pt-20  "
+          className="sm:w-[55%] w-screen px-4 sm:px-0 sm:float-right sm:pt-10 pt-4 lg:pt-20  "
         >
           {poster.slice(0, 3).map((item) => (
             <SplideSlide key={item._id}>
@@ -53,51 +64,111 @@ const Discover = async () => {
                 alt={item.name}
                 width={300}
                 height={300}
-                className="object-cover  h-[70%] rounded-lg"
+                className="object-cover h-[70%] rounded-lg"
               />
-              <p className="font-semibold">{item.name}</p>
-              <p className="text-sm font-bold">${item.price}</p>
+              <p className="font-semibold truncate">{item.name}</p>
+              <p className="text-sm font-bold ">${item.price}</p>
             </SplideSlide>
           ))}
         </Splide>
       </div>
 
-      <div className="flex justify-end flex-col ml-auto bg-gray-200 px-4 w-full">
-        <h1 className="sm:text-3xl text-xl lg:text-5xl">
+      <div className="sm:px-6 px-4 lg:px-10 flex sm:justify-end sm:text-right flex-col sm:ml-auto  w-full">
+        <h1 className="sm:text-3xl text-3xl lg:text-6xl font-bold">
           Products That
           <br />
           May Be of Interest
         </h1>
         <br />
-        <p className="pt-3">
+        <p className="pt-1 sm:pt-3 font-semibold">
           Exclusive Italian-made products for the comfort
           <br />
           and coziness of your daily life
         </p>
       </div>
 
-      {/* <div>
-        <div className="float-left w-1/2" key={poster[0]._id}>
-          <Image src={poster[0].poster} width={400} height={400} alt="img" />
-          <p>{poster[0].name}</p>
-          <p>{poster[0].price}</p>
+      <div className="pt-3 sm:pt-6 px-4 sm:px-10 ">
+        <div className="sm:float-left sm:w-1/2  " key={poster[4]._id}>
+          <Image
+            src={poster[4].poster}
+            width={350}
+            height={350}
+            alt="img"
+            className="sm:h-[90%] h-[50%] object-cover flex justify-center mx-auto sm:flex-none sm:justify-normal sm:mx-0  rounded-md"
+          />
+
+          <p className="font-semibold flex justify-start sm:flex-none sm:justify-normal sm:mx-0">
+            {poster[4].name}
+          </p>
+          <p className="text-sm font-bold flex justify-start sm:flex-none sm:justify-normal sm:mx-0">
+            ${poster[4].price}
+          </p>
         </div>
-        <div>
-          {poster.slice(-2).map((item) => (
-            <div className="float-right w-1/2" key={item._id}>
+        <div className="px-4 sm:px-6 flex pt-4 sm:pt-0 gap-2 ">
+          {poster.slice(5, 7).map((item) => (
+            <div className="sm:float-right  sm:w-1/2" key={item._id}>
               <Image
                 src={item.poster}
-                width={200}
-                height={200}
+                width={250}
+                height={250}
                 alt="img"
-                className="flex"
+                className="h-[70%] object-cover rounded-md"
               />
-              <p>{item.name}</p>
-              <p>{item.price}</p>
+              <p className="font-semibold truncate">{item.name}</p>
+              <p className="text-sm font-bold">${item.price}</p>
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
+
+      <div className="pt-3 sm:pt-6 px-4 sm:px-10 clear-both">
+        <div className="sm:float-right sm:w-[30%]" key={poster[8]._id}>
+          <Image
+            src={poster[8].poster}
+            width={300}
+            height={300}
+            alt="img"
+            className="h-[70%] object-cover flex justify-center mx-auto sm:flex-none sm:justify-normal sm:mx-0  rounded-md"
+          />
+          <p className="font-semibold flex justify-start  sm:flex-none sm:justify-normal  ">
+            {poster[8].name}
+          </p>
+          <p className="text-sm font-bold flex justify-start  sm:flex-none sm:justify-normal  ">
+            ${poster[8].price}
+          </p>
+        </div>
+        <div className="px-4 flex gap-2 pt-4 sm:pt-0">
+          {poster.slice(9, 11).map((item) => (
+            <div className="sm:float-left sm:w-[70%]" key={item._id}>
+              <Image
+                src={item.poster}
+                width={250}
+                height={250}
+                alt="img"
+                className="h-[70%] object-cover rounded-md"
+              />
+              <p className="font-semibold truncate">{item.name}</p>
+              <p className="text-sm font-bold">${item.price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="px-6 flex  gap-2  ">
+        {poster.slice(0, 2).map((item) => (
+          <div className="" key={item._id}>
+            <Image
+              src={item.poster}
+              width={250}
+              height={250}
+              alt="img"
+              className="h-[70%]  object-cover rounded-md"
+            />
+            <p className="font-semibold truncate">{item.name}</p>
+            <p className="text-sm font-bold">${item.price}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,19 +8,19 @@ const Navbar = () => {
     setNav(!nav);
   };
   return (
-    <div className="pt-1 px-3 sm:pt-4 sm:px-12 bg-hero bg-cover bg-center h-screen text-black">
+    <div className="pt-1 px-3 sm:pt-4 sm:px-12 bg-hero bg-cover bg-center sm:h-screen text-black">
       <div className="flex items-center font-bold">
         <div className="flex justify-start space-x-3">
           {!nav ? (
             <button>
-              <AiOutlineMenu size={30} onClick={handleClick} />
+              <AiOutlineMenu size={35} onClick={handleClick} />
             </button>
           ) : (
             <button>
-              <AiOutlineClose size={30} onClick={handleClick} />
+              <AiOutlineClose size={35} onClick={handleClick} />
             </button>
           )}
-          <h1 className="text-2xl">HIKLI</h1>
+          <h1 className="text-4xl">HIKLI</h1>
         </div>
         <div className="hidden sm:flex ml-auto space-x-6">
           <p>Search</p>
@@ -29,19 +29,19 @@ const Navbar = () => {
         </div>
       </div>
       <div className="pt-10 sm:pt-24">
-        <h1 className="text-6xl sm:text-8xl">Comfort</h1>
+        <h1 className="text-7xl sm:text-8xl">Comfort</h1>
         <h1 className="text-5xl sm:text-8xl">In Furniture</h1>
         <p className="font-medium pt-10 sm:pt-6 text-xl">
           See our thoughtfully curated collection of Furniture, lighting, and
           Objects
         </p>
-        <button className="font-bold px-4 py-2 mt-8 border rounded-md bg-teal-700 text-teal-50 text-xl">
+        <button className="sm:relative font-bold px-4 py-2 mt-8 border rounded-md bg-teal-600 text-teal-50 text-xl">
           View Collection
         </button>
-        {/* <p className="hidden sm:inline font-bold text-lg">
-          Functional, practical interior solutions that make life simple and
-          beautiful
-        </p> */}
+        <p className=" font-bold pt-6 sm:absolute sm:mb-10  sm:mr-14 sm:bottom-0 sm:right-0  text-base">
+          Functional, practical interior solutions
+          <br /> that make life simple and beautiful
+        </p>
       </div>
     </div>
   );
