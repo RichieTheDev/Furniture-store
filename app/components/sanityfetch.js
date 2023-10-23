12ab8a1f8a83c07d5eaa7197d7f50f9c107e7852
RichieTheDev <richiemomodu@gmail.com>
2023-10-23 17:30:04 +0100

    added new changess

--- app/components/sanityfetch.js
+++
@@ -1,6 +0,0 @@
-import { client } from "@/sanity/lib/client";
-import { groq } from "next-sanity";
-
-export const poster =
-  (await client.fetch) <
-  groq`*[_type=='furniture']{_id,name,price, "poster": poster.asset->url}`;
